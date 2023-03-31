2ef86b240f1686570f8b77da46b09ce67183f48f
Nathalieelizabeth <elizabethsnathalie@gmail.com>
2023-03-31 20:06:48 +0300

    first commit

---
+++ maths.py
@@ -0,0 +1,19 @@
+def add(a,b):
+    answer=a+b
+    return answer
+
+def subtract(e,f):
+    answer=e-f
+    return answer
+
+def divide(j,h):
+    answer=j/h
+    return answer
+
+def multiply(i,k):
+    answer=i*k
+    return answer
+
+def remainder(m,n):
+    answer=m%n
+    return answer
